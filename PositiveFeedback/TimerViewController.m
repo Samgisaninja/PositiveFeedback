2c5d38ee3ce0998f6786edc4dee2f8f06d57b11d
Sam Gardner <stgardner4@att.net>
2017-12-05 22:00:43 -0600

    Fixed massive bugs

--- PositiveFeedback/TimerViewController.m
+++ PositiveFeedback/TimerViewController.m
@@ -26,27 +26,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
--(void)updateSeconds{
+-(void)update{
     NSTimeInterval currentUnixTime = [[NSDate date] timeIntervalSince1970];
-    NSTimeInterval timerCountS = currentUnixTime - [relativeStartTime timeIntervalSince1970];
+    NSTimeInterval timerCountS = currentUnixTime - [startTime timeIntervalSince1970];
+    int minutesElapsed = (int) timerCountS / 60;
+    timerCountS -= minutesElapsed * 60;
     if (timerCountS < 10) {
         timerCountSLabel.text = [NSString stringWithFormat:@"0%.2f",timerCountS];
     } else {
         timerCountSLabel.text = [NSString stringWithFormat:@"%.2f",timerCountS];
     }
-}
--(void)updateMinutes{
-    NSTimeInterval currentUnixTime = [[NSDate date] timeIntervalSince1970];
-    NSTimeInterval timerCountS = currentUnixTime - [startTime timeIntervalSince1970];
-    NSInteger timerCountMInt = timerCountS;
-    NSInteger timerCountMIntDivided = timerCountMInt / 60;
-    if (timerCountMIntDivided < 10) {
-        timerCountMLabel.text = [NSString stringWithFormat:@"0%.1ld",(long)timerCountMIntDivided];
+    if (minutesElapsed < 10) {
+        timerCountMLabel.text = [NSString stringWithFormat:@"0%.1d",minutesElapsed];
     } else {
-        timerCountMLabel.text = [NSString stringWithFormat:@"%.1ld",(long)timerCountMIntDivided];
+        timerCountMLabel.text = [NSString stringWithFormat:@"%.1d",minutesElapsed];
     }
-    
-    relativeStartTime = [NSDate date];
 }
 -(IBAction)startTimer:(id)sender{
     if ([justInvalidated isEqualToString:@"YES"]) {
@@ -56,9 +50,7 @@
         relativeStartTime = [NSDate date];
         timerCountS = 0;
         timerCountMInt = 0;
-        secondsTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateSeconds) userInfo:nil repeats:YES];
-        minutesTimer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(updateMinutes) userInfo:nil repeats:YES];
-        [minutesTimer fire];
+        secondsTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(update) userInfo:nil repeats:YES];
         _releaseHintLabel.hidden = TRUE;
         self.view.backgroundColor = [UIColor darkGrayColor];
         [startUIButton setTitle:@"Stop" forState:UIControlStateNormal];
@@ -69,7 +61,6 @@
         NSLog(@"Touch Down ELSE");
         [secondsTimer fire];
         [secondsTimer invalidate];
-        [minutesTimer invalidate];
         justInvalidated = @"YES";
         [startUIButton setTitle:@"Hold and release to start" forState:UIControlStateNormal];
     } else {
@@ -80,10 +71,8 @@
 
 -(IBAction)resetTimer:(id)sender{
     [secondsTimer invalidate];
-    [minutesTimer invalidate];
     timerCountS = 0;
-    timerCountSLabel.text = [NSString stringWithFormat:@"0%.2f",timerCountS];
-    timerCountMLabel.text = [NSString stringWithFormat:@"0%.1ld",(long)timerCountMIntDivided];
+    timerCountSLabel.text = [NSString stringWithFormat:@"0%.2f", timerCountS];
     
 }
 @end
