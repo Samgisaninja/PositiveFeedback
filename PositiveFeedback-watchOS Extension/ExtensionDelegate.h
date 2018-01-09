e7b59c22cf09ef5ba8d4bbe85634f37ba60fbd03
Sam Gardner <stgardner4@att.net>
2018-01-08 19:09:10 -0600

    Added watchOS target :)

---
+++ PositiveFeedback-watchOS Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  PositiveFeedback-watchOS Extension
+//
+//  Created by Sam Gardner on 1/8/18.
+//  Copyright © 2018 Sam Gardner. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ PositiveFeedback-watchOS Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  PositiveFeedback-watchOS Extension
+//
+//  Created by Sam Gardner on 1/8/18.
+//  Copyright © 2018 Sam Gardner. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
