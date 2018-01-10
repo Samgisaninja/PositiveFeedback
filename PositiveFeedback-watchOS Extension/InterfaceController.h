9774959197cb09acc53b22f439c73abf1622e6a9
Sam Gardner <stgardner4@att.net>
2018-01-09 21:33:16 -0600

    this code looks familiar

--- PositiveFeedback-watchOS Extension/InterfaceController.h
+++ PositiveFeedback-watchOS Extension/InterfaceController.h
@@ -9,6 +9,24 @@
 #import <WatchKit/WatchKit.h>
 #import <Foundation/Foundation.h>
 
-@interface InterfaceController : WKInterfaceController
-
+@interface InterfaceController : WKInterfaceController{
+    IBOutlet WKInterfaceLabel *timerCountSLabel;
+    IBOutlet WKInterfaceLabel *timerCountMLabel;
+    NSTimeInterval *timeElapsed;
+    NSTimer *secondsTimer;
+    NSDate *startTime;
+    NSDate *relativeStartTime;
+    IBOutlet WKInterfaceButton *startWKInterfaceButton;
+    NSString *justInvalidated;
+    int intervalsPassed;
+}
+-(void)update;
+-(void)resetTimer;
+// @property (strong, nonatomic) IBOutlet UIProgressView *progressBar;
+-(IBAction)startTimer:(id)sender;
+@property (strong, nonatomic) NSNumber *goalMinutes;
+@property (strong, nonatomic) NSNumber *goalSeconds;
+@property (strong, nonatomic) NSNumber *runDistance;
+@property float goalTime;
+@property float goalInterval;
 @end
--- PositiveFeedback-watchOS Extension/InterfaceController.m
+++ PositiveFeedback-watchOS Extension/InterfaceController.m
@@ -18,8 +18,15 @@
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
-
-    // Configure interface objects here.
+    //viewDidntLoad!
+    timerCountSLabel.text = [NSString stringWithFormat:@"00.00"];
+    timerCountMLabel.text = [NSString stringWithFormat:@"00"];
+    timeElapsed = 0;
+    intervalsPassed = 0;
+    /* _goalTime = [goalSeconds floatValue] + [goalMinutes floatValue] * 60;
+    _goalInterval = _goalTime / [runDistance floatValue];
+    self.progressBar.progress = 0; */
+    
 }
 
 - (void)willActivate {
@@ -29,7 +36,64 @@
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
-    [super didDeactivate];
+}
+
+-(void)update{
+    NSTimeInterval currentUnixTime = [[NSDate date] timeIntervalSince1970];
+    NSTimeInterval timeElapsed = currentUnixTime - [startTime timeIntervalSince1970];
+    int minutesElapsed = (int) timeElapsed / 60;
+    timeElapsed -= minutesElapsed * 60;
+    if (timeElapsed < 10) {
+        timerCountSLabel.text = [NSString stringWithFormat:@"0%.2f",timeElapsed];
+    } else {
+        timerCountSLabel.text = [NSString stringWithFormat:@"%.2f",timeElapsed];
+    }
+    if (minutesElapsed < 10) {
+        timerCountMLabel.text = [NSString stringWithFormat:@"0%.1d",minutesElapsed];
+    } else {
+        timerCountMLabel.text = [NSString stringWithFormat:@"%.1d",minutesElapsed];
+    }
+    /* float progressToInterval = currentUnixTime - [relativeStartTime timeIntervalSince1970];
+    if (progressToInterval > _goalInterval) {
+        relativeStartTime = [NSDate date];
+        intervalsPassed = intervalsPassed + 1;
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    }
+    self.progressBar.progress = timeElapsed / _goalTime;
+    if (timeElapsed > _goalTime) {
+        [self resetTimer];
+    }
+    */
+}
+-(IBAction)startTimer:(id)sender{
+    if ([justInvalidated isEqualToString:@"YES"]) {
+        justInvalidated = @"NO";
+    } else {
+        startTime = [NSDate date];
+        relativeStartTime = [NSDate date];
+        timeElapsed = 0;
+        secondsTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(update) userInfo:nil repeats:YES];
+        [startWKInterfaceButton setTitle:@"Stop"];
+    }
+}
+
+- (IBAction)startTouchDown:(id)sender {
+    if ([secondsTimer isValid]) {
+        [secondsTimer fire];
+        [secondsTimer invalidate];
+        justInvalidated = @"YES";
+        [startWKInterfaceButton setTitle:@"Hold and release to start"];
+    } else {
+    }
+}
+
+-(void)resetTimer{
+    [secondsTimer invalidate];
+    timeElapsed = 0;
+    timerCountSLabel.text = [NSString stringWithFormat:@"00.00"];
+    timerCountMLabel.text = [NSString stringWithFormat:@"00"];
+    //self.progressBar.progress = 0;
+    [startWKInterfaceButton setTitle:@"Hold and release to start"];
 }
 
 @end
