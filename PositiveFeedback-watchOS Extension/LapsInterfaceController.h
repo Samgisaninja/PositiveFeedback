95ad229dfaed35183a283aaa885f95149134c57e
Sam Gardner <stgardner4@att.net>
2018-09-03 00:11:19 -0500

    Got rid of Distance picker and added more logical laps picker

---
+++ PositiveFeedback-watchOS Extension/LapsInterfaceController.h
@@ -0,0 +1,18 @@
+//
+//  LapsInterfaceController.h
+//  PositiveFeedback-watchOS Extension
+//
+//  Created by Sam Gardner on 8/26/18.
+//  Copyright © 2018 Sam Gardner. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+float runDistanceFloat;
+@interface LapsInterfaceController : WKInterfaceController
+@property (strong, nonatomic) NSNumber *runDistance;
+@property (strong, nonatomic) IBOutlet WKInterfaceLabel *runDistanceLabel;
+
+@end
+
---
+++ PositiveFeedback-watchOS Extension/LapsInterfaceController.m
@@ -0,0 +1,47 @@
+//
+//  LapsInterfaceController.m
+//  PositiveFeedback-watchOS Extension
+//
+//  Created by Sam Gardner on 8/26/18.
+//  Copyright © 2018 Sam Gardner. All rights reserved.
+//
+
+#import "LapsInterfaceController.h"
+
+@implementation LapsInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    runDistanceFloat = 4.0;
+    _runDistance = @(4.0);
+    [[self runDistanceLabel] setText:[NSString stringWithFormat:@"%.f", roundf(runDistanceFloat)]];
+}
+
+-(IBAction)plusButton:(id)sender{
+    runDistanceFloat = runDistanceFloat + 1.0;
+    _runDistance = @(roundf(runDistanceFloat));
+    [[self runDistanceLabel] setText:[NSString stringWithFormat:@"%.f", roundf(runDistanceFloat)]];
+}
+
+-(IBAction)minusButton:(id)sender{
+    runDistanceFloat = runDistanceFloat -1.0;
+    _runDistance = @(roundf(runDistanceFloat));
+    [[self runDistanceLabel] setText:[NSString stringWithFormat:@"%.f", roundf(runDistanceFloat)]];
+}
+
+-(IBAction)nextButton:(id)sender{
+    NSDictionary * goalDataShare = @{@"runDistance" : _runDistance};
+    [self pushControllerWithName:@"TimePickerInterfaceController" context:goalDataShare];
+}
+
+-(IBAction)fiveKButton:(id)sender{
+    _runDistance = @(3.125);
+    NSDictionary * goalDataShare = @{@"runDistance" : _runDistance};
+    [self pushControllerWithName:@"TimePickerInterfaceController" context:goalDataShare];
+}
+
+-(IBAction)endlessButton:(id)sender{
+    _runDistance = @(-1.0);
+    NSDictionary * goalDataShare = @{@"runDistance" : _runDistance};
+    [self pushControllerWithName:@"TimePickerInterfaceController" context:goalDataShare];
+}
+@end
